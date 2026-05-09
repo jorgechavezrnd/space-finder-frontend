3d5d2cd7a76fbef65a0923609b00321d6fa97736
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-09 13:35:40 -0400

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import { useState } from 'react';
 import './App.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <>
-      <section id="center">
-        <h1>App works!</h1>
-      </section>
-    </>
-  )
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName}/>
+          <div className="wrapper">
+            <Outlet />
+          </div>
+        </>
+      ),
+      children:[
+        {
+          path: "/",
+          element: <div>Hello world!</div>,
+        },
+        {
+          path: "/login",
+          element: <div>Login page</div>,
+        },
+        {
+          path: "/profile",
+          element: <div>Profile page</div>,
+        },
+        {
+          path: "/createSpace",
+          element: <div>Create space page</div>,
+        },
+        {
+          path: "/spaces",
+          element: <div>Spaces page</div>,
+        },
+      ]
+    },
+  ]);
+
+  return <RouterProvider router={router} />
 }
 
 export default App
