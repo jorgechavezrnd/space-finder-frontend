b79712f3bfb2b6f7a479a00efe26c916d37d20b0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-15 23:42:11 -0400

    Add missing implementation in AuthService.ts

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import { type SignInOutput, fetchAuthSession, signIn } from '@aws-amplify/auth';
+import { type AuthUser, type SignInOutput, fetchAuthSession, getCurrentUser, signIn } from '@aws-amplify/auth';
 import { AuthStack } from '../../../space-finder/outputs.json';
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
@@ -17,7 +17,7 @@ Amplify.configure({
 });
 
 export class AuthService {
-  private user: SignInOutput | undefined;
+  private user: SignInOutput | AuthUser | undefined;
   private userName: string = '';
   private jwtToken: string | undefined;
   private temporaryCredentials: object | undefined;
@@ -32,21 +32,37 @@ export class AuthService {
 
   public async login(userName: string, password: string): Promise<Object | undefined> {
     try {
-      const signInOutput: SignInOutput = await signIn({
-        username: userName,
-        password: password,
-        options: {
-          authFlowType: 'USER_PASSWORD_AUTH'
-        }
-      });
+      // check if user is already logged in
+      const user = await this.getCurrentUser();
+      if (user) {
+        this.user = user;
+      } else {
+        const signInOutput: SignInOutput = await signIn({
+          username: userName,
+          password: password,
+          options: {
+            authFlowType: 'USER_PASSWORD_AUTH'
+          }
+        });
 
-      this.user = signInOutput;
+        this.user = signInOutput;
+      }
+
       this.userName = userName;
       await this.generateIdToken();
 
       return this.user;
     } catch (error) {
       console.error(error);
+      return undefined;
+    }
+  }
+
+  public async getCurrentUser() {
+    try {
+      const user = await getCurrentUser();
+      return user;
+    } catch (error) {
       return undefined;
     }
   }
@@ -62,14 +78,14 @@ export class AuthService {
 
   private async generateTemporaryCredentials() {
     const idToken = this.getIdToken();
-    const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/us-east-1_rgHoEDiOP`;
+    const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/${AuthStack.SpaceUserPoolId}`;
     const cognitoIdentity = new CognitoIdentityClient({
       credentials: fromCognitoIdentityPool({
         clientConfig: {
           region: awsRegion
         },
-        identityPoolId: 'us-east-1:8d2ddcad-078b-4ed1-9f4a-1461cfc52543',
-        'logins': {
+        identityPoolId: AuthStack.SpaceIdentityPoolId,
+        logins: {
           [cognitoIdentityPool]: idToken!
         }
       })
